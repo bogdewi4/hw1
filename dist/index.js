dd6b3749f2ccb2263c3a59cf4b20c366991ce420
Aleh Bahdzevich <aleh.bogdevich@mediapark.com>
2023-12-05 22:20:19 +0300

    add dist to gitignore file

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const setting_1 = require("./setting");
-const PORT = 3000;
-setting_1.app.listen(PORT, () => {
-    console.log(`App start on port: ${PORT}`);
-});
