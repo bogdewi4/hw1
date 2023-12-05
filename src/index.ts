789d315c05d14588abb9c76d77c112cdfca6186f
Aleh Bahdzevich <aleh.bogdevich@mediapark.com>
2023-12-05 23:14:21 +0300

    fix 2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,98 @@
-import { app } from './setting';
+import express, { Response, json } from 'express';
+import {
+  CreateVideo,
+  Error,
+  RequestWithBody,
+  RequestWithParams,
+  VideoDB,
+  resolutions,
+  videos,
+} from './setting';
+
+export const app = express();
+
+app.use(json());
 
 const PORT = 5000;
+
+app.get('/videos', (_req, res) => {
+  res.send(videos);
+});
+
+app.get('/videos/:id', (req: RequestWithParams<{ id: string }>, res) => {
+  const videoId = +req.params.id;
+
+  const findedVideo = videos.find(({ id }) => id === videoId);
+
+  if (!findedVideo) {
+    res.sendStatus(404);
+    return;
+  }
+
+  res.send(findedVideo);
+});
+
+app.post('/videos', (req: RequestWithBody<CreateVideo>, res: Response) => {
+  let error: Error = {
+    errorMessages: [],
+  };
+
+  let { title, author, availableResolutions } = req.body;
+
+  if (
+    !title ||
+    typeof title !== 'string' ||
+    !title.trim() ||
+    title.trim().length > 40
+  ) {
+    error.errorMessages.push({ message: 'Invalid title!', field: 'title' });
+  }
+  if (
+    !author ||
+    typeof author !== 'string' ||
+    !author.trim() ||
+    author.trim().length > 20
+  ) {
+    error.errorMessages.push({ message: 'Invalid author!', field: 'author' });
+  }
+
+  if (availableResolutions && Array.isArray(availableResolutions)) {
+    availableResolutions.forEach((resolution) => {
+      !resolutions.includes(resolution) &&
+        error.errorMessages.push({
+          message: 'Invalid availableResolutions!',
+          field: 'availableResolutions',
+        });
+    });
+  } else {
+    availableResolutions = [];
+  }
+
+  if (error.errorMessages.length) {
+    res.status(400).send(error);
+    return;
+  }
+
+  const createdAt = new Date();
+  const publicationDate = new Date();
+
+  publicationDate.setDate(createdAt.getDate() + 1);
+
+  const newVideo: VideoDB = {
+    id: +new Date(),
+    canBeDownloaded: false,
+    minAgeRestriction: null,
+    createdAt: createdAt.toISOString(),
+    publicationDate: publicationDate.toISOString(),
+    title,
+    author,
+    availableResolutions,
+  };
+
+  videos.push(newVideo);
+
+  res.status(201).send(newVideo);
+});
 
 app.listen(PORT, () => {
   console.log(`App start on port: ${PORT}`);
--- src/setting.ts
+++ src/setting.ts
@@ -1,10 +1,6 @@
-import express, { Request, Response, json } from 'express';
+import { Request, Response } from 'express';
 
-export const app = express();
-
-app.use(json());
-
-const resolutions = [
+export const resolutions = [
   'P144',
   'P240',
   'P360',
@@ -15,7 +11,7 @@ const resolutions = [
   'P2160',
 ];
 
-type VideoDB = {
+export type VideoDB = {
   id: number;
   title: string;
   author: string;
@@ -26,25 +22,25 @@ type VideoDB = {
   availableResolutions: typeof resolutions;
 };
 
-type RequestWithParams<T> = Request<T, {}, {}, {}, {}>;
-type RequestWithBody<T> = Request<{}, {}, T, {}, {}>;
+export type RequestWithParams<T> = Request<T, {}, {}, {}, {}>;
+export type RequestWithBody<T> = Request<{}, {}, T, {}, {}>;
 
-type CreateVideo = {
+export type CreateVideo = {
   title: string;
   author: string;
   availableResolutions: typeof resolutions;
 };
 
-type ErrorMessage = {
+export type ErrorMessage = {
   message: string;
   field: string;
 };
 
-type Error = {
+export type Error = {
   errorMessages: ErrorMessage[];
 };
 
-const videos: VideoDB[] = [
+export const videos: VideoDB[] = [
   {
     id: 0,
     title: 'string',
@@ -56,82 +52,3 @@ const videos: VideoDB[] = [
     availableResolutions: ['P144'],
   },
 ];
-
-app.get('/videos', (_req, res) => {
-  res.send(videos);
-});
-
-app.get('/videos/:id', (req: RequestWithParams<{ id: string }>, res) => {
-  const videoId = +req.params.id;
-
-  const findedVideo = videos.find(({ id }) => id === videoId);
-
-  if (!findedVideo) {
-    res.sendStatus(404);
-    return;
-  }
-
-  res.send(findedVideo);
-});
-
-app.post('/videos', (req: RequestWithBody<CreateVideo>, res: Response) => {
-  let error: Error = {
-    errorMessages: [],
-  };
-
-  let { title, author, availableResolutions } = req.body;
-
-  if (
-    !title ||
-    typeof title !== 'string' ||
-    !title.trim() ||
-    title.trim().length > 40
-  ) {
-    error.errorMessages.push({ message: 'Invalid title!', field: 'title' });
-  }
-  if (
-    !author ||
-    typeof author !== 'string' ||
-    !author.trim() ||
-    author.trim().length > 20
-  ) {
-    error.errorMessages.push({ message: 'Invalid author!', field: 'author' });
-  }
-
-  if (availableResolutions && Array.isArray(availableResolutions)) {
-    availableResolutions.forEach((resolution) => {
-      !resolutions.includes(resolution) &&
-        error.errorMessages.push({
-          message: 'Invalid availableResolutions!',
-          field: 'availableResolutions',
-        });
-    });
-  } else {
-    availableResolutions = [];
-  }
-
-  if (error.errorMessages.length) {
-    res.status(400).send(error);
-    return;
-  }
-
-  const createdAt = new Date();
-  const publicationDate = new Date();
-
-  publicationDate.setDate(createdAt.getDate() + 1);
-
-  const newVideo: VideoDB = {
-    id: +new Date(),
-    canBeDownloaded: false,
-    minAgeRestriction: null,
-    createdAt: createdAt.toISOString(),
-    publicationDate: publicationDate.toISOString(),
-    title,
-    author,
-    availableResolutions,
-  };
-
-  videos.push(newVideo);
-
-  res.status(201).send(newVideo);
-});
