91c6207e43aea63d08fed394926e97fd4273d737
Aleh Bahdzevich <aleh.bogdevich@mediapark.com>
2023-12-07 01:08:13 +0300

    drop table

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,17 +1,6 @@
 import { VideoDB } from '../model';
 
-let videos: VideoDB[] = [
-  {
-    id: 0,
-    title: 'string',
-    author: 'string',
-    canBeDownloaded: true,
-    minAgeRestriction: null,
-    createdAt: '2023-12-04T16:26:41.674Z',
-    publicationDate: '2023-12-04T16:26:41.674Z',
-    availableResolutions: ['P144'],
-  },
-];
+let videos: VideoDB[] = [];
 
 const replaceVideos = (mutatedVideos: VideoDB[]) => {
   videos = [...mutatedVideos];
