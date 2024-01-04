d89a5a5f1ca4666c14703f82591c81b7ff4c5603
Aleh Bahdzevich <aleh.bogdevich@mediapark.com>
2024-01-04 23:57:12 +0300

    remove extra auth validation

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,7 @@
 import { Router } from 'express';
 
-import { authMiddleware } from '../middlewares/auth';
 import { AuthController } from '../features/auth';
 
 export const authRoute = Router();
 
-authRoute.post('/login', authMiddleware, AuthController.loginUser);
+authRoute.post('/login', AuthController.loginUser);
